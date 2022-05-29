e3f401845eb522c30ea7d71be8c70b4d2cad5ce8
OrderWOPower <82205477+OrderWOPower@users.noreply.github.com>
2022-05-30 00:05:44 +0800

    Add files via upload

---
+++ LessInterracialMarriageModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.GameComponents;
+
+namespace LessInterracialMarriage
+{
+    public class LessInterracialMarriageModel : DefaultMarriageModel
+    {
+        // If 10% or more of the lords in either hero's faction is of a foreign culture, stop the couple from marrying.
+        public override bool IsCoupleSuitableForMarriage(Hero firstHero, Hero secondHero)
+        {
+            if (firstHero.MapFaction.Culture != secondHero.MapFaction.Culture)
+            {
+                List<Hero> lords = firstHero.MapFaction.Lords.ToList();
+                List<Hero> lords2 = secondHero.MapFaction.Lords.ToList();
+                if (lords.Count(lord => lord.Culture != firstHero.MapFaction.Culture) >= lords.Count / 10 || lords2.Count(lord => lord.Culture != secondHero.MapFaction.Culture) >= lords2.Count / 10)
+                {
+                    return false;
+                }
+            }
+            return base.IsCoupleSuitableForMarriage(firstHero, secondHero);
+        }
+    }
+}
---
+++ LessInterracialMarriageSubModule.cs
@@ -0,0 +1,19 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace LessInterracialMarriage
+{
+    public class LessInterracialMarriageSubModule : MBSubModuleBase
+    {
+        // This mod stops nobles of different cultures from marrying if their factions have too many foreign nobles.
+        protected override void OnGameStart(Game game, IGameStarter gameStarter)
+        {
+            if (game.GameType is Campaign)
+            {
+                CampaignGameStarter campaignStarter = (CampaignGameStarter)gameStarter;
+                campaignStarter.AddModel(new LessInterracialMarriageModel());
+            }
+        }
+    }
+}
