8ab54128f75a02895f22adb607642897dead3578
OrderWOPower <82205477+OrderWOPower@users.noreply.github.com>
2022-06-17 21:02:26 +0800

    Add files via upload

--- LessInterracialMarriageModel.cs
+++ LessInterracialMarriageModel.cs
@@ -10,11 +10,13 @@ namespace LessInterracialMarriage
         // If 10% or more of the lords in either hero's faction is of a foreign culture, stop the couple from marrying.
         public override bool IsCoupleSuitableForMarriage(Hero firstHero, Hero secondHero)
         {
-            if (firstHero.MapFaction.Culture != secondHero.MapFaction.Culture)
+            CultureObject culture = firstHero.MapFaction.Culture;
+            CultureObject culture2 = secondHero.MapFaction.Culture;
+            if (culture != culture2)
             {
                 List<Hero> lords = firstHero.MapFaction.Lords.ToList();
                 List<Hero> lords2 = secondHero.MapFaction.Lords.ToList();
-                if (lords.Count(lord => lord.Culture != firstHero.MapFaction.Culture) >= lords.Count / 10 || lords2.Count(lord => lord.Culture != secondHero.MapFaction.Culture) >= lords2.Count / 10)
+                if (lords.Count(lord => lord.Culture != culture) >= lords.Count / 10 || lords2.Count(lord => lord.Culture != culture2) >= lords2.Count / 10)
                 {
                     return false;
                 }
